8df76dc41c7aa06e88a57c47fc8fec3c76719bfe
Mike Ng <ekimng@outlook.com>
2016-06-25 22:58:32 -0400

    initial release, trying npm out

---
+++ index.js
@@ -0,0 +1,30 @@
+exports.getBirthstone = function (month) {
+    switch (month) {
+        case 1:
+            return "garnet";
+        case 2:
+            return "amethyst";
+        case 3:
+            return "aquamarine";
+        case 4:
+            return "diamond";
+        case 5:
+            return "emerald";
+        case 6:
+            return "pearl";
+        case 7:
+            return "ruby";
+        case 8:
+            return "peridot";
+        case 9:
+            return "sapphire";
+        case 10:
+            return "opal";
+        case 11:
+            return "citrine";
+        case 12:
+            return "topaz";
+        default:
+            return "Invalid month."
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,14 @@
+var birthstone = require('..');
+
+console.log(birthstone.getBirthstone(1));
+console.log(birthstone.getBirthstone(2));
+console.log(birthstone.getBirthstone(3));
+console.log(birthstone.getBirthstone(4));
+console.log(birthstone.getBirthstone(5));
+console.log(birthstone.getBirthstone(6));
+console.log(birthstone.getBirthstone(7));
+console.log(birthstone.getBirthstone(8));
+console.log(birthstone.getBirthstone(9));
+console.log(birthstone.getBirthstone(10));
+console.log(birthstone.getBirthstone(11));
+console.log(birthstone.getBirthstone(12));
